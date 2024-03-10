24cb3f7d2709a47423fa50455bc8e880b58c7741
= <ryanneilparker@gmail.com>
2024-03-10 08:43:58 +0200

    Add basic landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Landing Page</h1>
+
+<h2>Links</h2>
+<ul>
+  <li><a href="/login">Login Page</a></li>
+  <li><a href="/prompt">Prompt Page</a></li>
+</ul>
